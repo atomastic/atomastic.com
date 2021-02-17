b3041336b16a867bc7b6d822545a563d39267a8c
Awilum <awilum@msn.com>
2021-02-18 01:05:51 +0300

    update tailwind confing

--- project/themes/noir/tailwind.config.js
+++ project/themes/noir/tailwind.config.js
@@ -13,10 +13,6 @@ module.exports = {
             '5xl': '3rem',
             '6xl': '4rem',
             '7xl': '5rem'
-        },
-        borderRadius: {
-            'full': '9999px',
-            'lg': '10px'
         }
     },
     variants: {
